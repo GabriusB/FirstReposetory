0f377adf0b0bd646bdb93838794a9f29496417d5
GabriusB <gabrielius2012@gmail.com>
2021-11-29 11:48:55 +0200

    second try

--- Github/git1.js
+++ Github/git1.js
@@ -1,1 +1,14 @@
-console.log("pirmas komitas")
+const btn = document.getElementById('button');
+const div = document.getElementById('div');
+
+function Random (){
+    const max = 100;
+    return Math.round(Math.random() * max );
+}
+
+
+
+btn.onclick = function() {
+    div.innerHTML = Random();
+}
+
